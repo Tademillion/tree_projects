3172be09bf27ac3736e1470e3beba979b0f926b0
taddemillion <tedlamillionyou@gmail.com>
2025-04-09 11:51:38 +0300

    add main header with all styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,18 @@
 //  hey this is mastry of javascript with the html and css
 //  all about dom amnipulations
 
-const element = document.getElementById("sidebar-content");
+// Select all elements with the class 'sidebar-content'
+const elements = document.querySelectorAll(".sidebar-content");
 
-element.addEventListener("mouseenter", () => {
-  element.style.backgroundColor = "red";
-});
+// Loop through each element and add event listeners
+elements.forEach((element) => {
+  element.addEventListener("mouseenter", () => {
+    element.style.backgroundColor = "red";
+  });
 
-element.addEventListener("mouseleave", () => {
-  element.style.backgroundColor = "black";
+  element.addEventListener("mouseleave", () => {
+    element.style.backgroundColor = "black";
+  });
 });
+//  increase the icon size when hover the icons
+//  there are also several events like  mouseup and mousedown mousemove mouseover...
